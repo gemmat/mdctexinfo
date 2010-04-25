5a2f723c65505f9f92a4ee532d05cb54cdb15054
Teruaki Gemma <teruakigemma@gmail.com>
2010-04-25 15:11:49 +0900

    it works.

--- xml2texi.scm
+++ xml2texi.scm
@@ -1,4 +1,5 @@
 (use srfi-1)
+(use srfi-13)
 (use file.util)
 (use gauche.parseopt)
 (use sxml.ssax)
@@ -22,12 +23,6 @@
                        #/\}/
                        "@}"))
 
-(define (pre node)
-  `(texinfo
-    "@example\n"
-    ,(sxml:string-value node)
-    "@end example"))
-
 (define (h2 node)
   `(texinfo "@heading " ,(sxml:string-value node)))
 
@@ -46,6 +41,12 @@
 (define (bold node)
   `(texinfo "@strong{" ,(sxml:string-value node) "}"))
 
+(define (pre node)
+  `(texinfo
+    "\n@example\n"
+    ,(sxml:string-value node)
+    "@end example\n"))
+
 (define (dl node)
   `(texinfo
     "\n@table @code\n"
@@ -55,7 +56,7 @@
                       ((xhtml:dd) `(,(sxml:string-value elt) "\n"))
                       (else       `("warning:" ,(sxml:string-value elt)))))
                   (sxml:child-elements node))
-    "@end table"))
+    "\n@end table\n"))
 
 (define (ul node)
   `(texinfo
@@ -65,7 +66,7 @@
                ((xhtml:li) (string-append "@item " (sxml:string-value elt) "\n"))
                (else       (string-append "warning:" (sxml:string-value elt)))))
            (sxml:child-elements node))
-    "@end itemize\n"))
+    "\n@end itemize\n"))
 
 (define (ol node)
   `(texinfo
@@ -75,39 +76,91 @@
                ((xhtml:li) (string-append "@item " (sxml:string-value elt) "\n"))
                (else       (string-append "warning:" (sxml:string-value elt)))))
            (sxml:child-elements node))
-    "@end enumerate\n"))
+    "\n@end enumerate\n"))
 
 (define (table node)
-  `(texinfo
-    "\n@multitable\n"
-    ,@(append-map (lambda (tr)
-                    (cons "@item "
-                          (cdr
-                           (append-map (lambda (td)
-                                         `("@tab " ,(sxml:string-value td) "\n"))
-                                       ((sxpath '(// (or@ xhtml:td xhtml:th))) tr)))))
-                  ((sxpath '(// xhtml:tr)) node))
-    "@end multitable"))
+  (define (column-length node)
+    (apply max (map (lambda (tr)
+                      (length ((sxpath '(// (or@ xhtml:th xhtml:td))) tr)))
+                    ((sxpath '(// xhtml:tr)) node))))
+
+  (let* ((col (column-length node))
+         (fractions (if (= col 1)
+                      "1"
+                      (string-join (make-list col (format #f ".~d" (quotient 100 col))) " "))))
+    `(texinfo
+      "\n@multitable @columnfractions " ,fractions "\n"
+      ,@(append-map (lambda (tr)
+                      (cons "@item "
+                            (cdr
+                             (append-map (lambda (td)
+                                           `("@tab " ,(sxml:string-value td) "\n"))
+                                         ((sxpath '(// (or@ xhtml:td xhtml:th))) tr)))))
+                    ((sxpath '(// xhtml:tr)) node))
+      "\n@end multitable\n")))
 
 (define (code node)
   `(texinfo "@code{" ,(sxml:string-value node) "}"))
 
 (define (ahref node)
   (let ((href ((if-car-sxpath '(@ href *text*)) node))
-        (body ((if-car-sxpath '(*text*)) node)))
-    (cond
-     ((and href body)
-      `(texinfo "@uref{" ,href "," ,body "} "))
-     (href
-      `(texinfo "@uref{" ,href "} "))
-     (else
-      ()))))
+        (body ((if-car-sxpath '(*text*)) node))
+        (rel  (or ((if-car-sxpath '(@ rel *text*)) node) "external")))
+    (let* ((bound (rxmatch-cond
+                    ((#/internal/ rel)
+                     (#f)
+                     'internal)
+                    ((#/external/ rel)
+                     (#f)
+                     'external)
+                    (else
+                     'external)))
+           (cmd (case bound
+                  ((internal) "@xref{")
+                  ((external) "@uref{")))
+           (link (case bound
+                   ((internal) (or (file-path->texinfo-node href) "Top"))
+                   ((external) href)))
+           (brace (case bound
+                    ((internal) "}. ")
+                    ((external) "} "))))
+      (cond
+       ((and href body)
+        `(texinfo ,cmd ,link "," ,body ,brace))
+       (href
+        `(texinfo ,cmd ,link ,brace))
+       (else
+        ())))))
 
 (define (div node)
   (or (and-let* ((class (sxml:attr node 'class))
                  ((string=? class "note")))
         `(texinfo "@footnote{" ,(sxml:string-value node) "}\n"))
       node))
+
+(define (file-path->texinfo-node path)
+  (define (check str)
+    (if (hash-table-exists? notfound str)
+      #f
+      str))
+
+  (define (capitalize str)
+    (string-join (map (lambda (x)
+                        (if (string=? x "ja")
+                          x
+                          (string-titlecase x)))
+                      (string-split str "/"))
+                 "/"))
+  (and
+   (string-scan path "developer.mozilla.org/ja/Core_JavaScript_1.5_Reference")
+   (rxmatch-cond
+     ((#/developer\.mozilla\.org\/(.*)\.html$/ path)
+      (#f node)
+      (check (capitalize node)))
+     ((#/developer\.mozilla\.org\/(.*)/ path)
+      (#f node)
+      (check (capitalize node)))
+     (else #f))))
 
 (define (process path prefix)
   (define (save-to path)
@@ -127,6 +180,7 @@
       (with-output-to-file save-file
         (lambda ()
           (let ((sxml (load-xml path)))
+            (print "@node " (file-path->texinfo-node path) ",,,Top")
             (print "@section " (sxml:string-value (getElementById "title" sxml)))
             (let1 debug-sxml
                 (pre-post-order
@@ -156,6 +210,7 @@
                     (write debug-sxml out)))))))))))
 
 (define verbose #f)
+(define notfound (make-hash-table 'string=?))
 
 (define (main args)
   (let-args (cdr args)
@@ -164,6 +219,9 @@
        (help   "h|help" => (cut show-help (car args)))
        . restargs)
     (set! verbose v)
+    (for-each (lambda (x)
+                (hash-table-put! notfound x #t))
+              (file->string-list "./notfound"))
     (for-each (cut process <> p) restargs))
   0)
 
